62dda889fb0f5ab7e0e27c91031c6a3513bcbc6f
Sky Crystal Admirer <121347435+huanmengyishi@users.noreply.github.com>
2025-03-30 18:27:40 +0800

    Update index.js
    修改bug

--- src/router/index.js
+++ src/router/index.js
@@ -105,6 +105,10 @@ const router = createRouter({
                                 }
                             }]
                         },
+                        {
+                            path: '/redirect/:path(.*)',
+                            component: () => import('@/views/RedirectView.vue')
+                        },
 
                     ]
                 }
